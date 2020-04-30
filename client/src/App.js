3c29efa858ec6fad7faea009e7d4c3eec9f0e530
Kody Yang <kodyyyang@gmail.com>
2020-04-30 17:05:08 -0400

    refactor function component to class component and update mismatched variable name

--- client/src/App.js
+++ client/src/App.js
@@ -2,55 +2,62 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-handleSubmit = async e => {
-  e.preventDefault();
-  const response = await fetch('/api/world', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ post: this.state.post }),
-  });
-  const body = await response.text();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {response: null};
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+    
+    this.setState({ response: body });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
   
-  this.setState({ responseToPost: body });
-};
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-
-      <p>{this.state.response}</p>
-      <form onSubmit={this.handleSubmit}>
-        <p>
-          <strong>Post to Server:</strong>
-        </p>
-        <input
-          type="text"
-          value={this.state.post}
-          onChange={e => this.setState({ post: e.target.value })}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <p>{this.state.responseToPost}</p>
-
-    </div>
-  );
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
+  
+      </div>
+    );
+  }
 }
 
 export default App;
